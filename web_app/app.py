9f087f9f95c2f1ebf1751a771dd000f2e81c455b
Edudeiko <evgeniy.dudeyko@gmail.com>
2020-02-14 23:47:45 -0500

    add basilica_service.py

--- web_app/app.py
+++ web_app/app.py
@@ -1,95 +1,37 @@
-from flask import Flask, jsonify, request, render_template
+import os
+from dotenv import load_dotenv
+
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///web_app_331.db"
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+from web_app.models import db, User, Tweet, migrate
+from web_app.routes import my_routes
+# from web_app.more_routes import more_routes
+from web_app.twitter_service import twitter_api_client
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL", default="OOPS")
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+    app.config["TWITTER_API_CLIENT"] = twitter_api_client()
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    app.register_blueprint(my_routes)
+    # app.register_blueprint(more_routes)
+
+    return app
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128))
 
-class Tweet(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    status = db.Column(db.String)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-#
-# ROUTING
-#
-
-@app.route("/")
-def index():
-    # return "Hello World!"
-    return render_template("homepage.html")
-
-@app.route("/about")
-def about():
-    return "About Me"
-
-@app.route("/users")
-@app.route("/users.json")
-def users():
-    # users = [
-    #     {"id":1, "name": "First User"},
-    #     {"id":2, "name": "Second User"},
-    #     {"id":3, "name": "Third User"},
-    # ]
-    # return jsonify(users)
-    users = User.query.all() # returns a list of <class 'alchemy.User'>
-    # print(type(users))
-    # print(type(users[0]))
-    # print(len(users))
-
-    users_response = []
-    for u in users:
-        user_dict = u.__dict__
-        del user_dict["_sa_instance_state"]
-        users_response.append(user_dict)
-
-    return jsonify(users_response)
-
-
-@app.route("/users/create", methods=["POST"])
-def create_user():
-    print("CREATING A NEW USER...")
-    print("FROM DATA:", dict(request.form))
-    # todo: create a new user
-    # return jsonify({"message": "CREATED OK(TODO)"})
-    if "name" in request.form:
-        name = request.form["name"]
-        print(name)
-        db.session.add(User(name=name))
-        db.session.commit()
-        return jsonify({"message": "CREATED OK", "name": name})
-    else:
-        return jsonify({"message": "OOPS PLEASE SPECIFY A NAME"})
-
-
-    #     print(jsonify({"message": "CREATED OK", "name": name}))
-    #     return render_template('new_user_created.html')
-    # else:
-    #     print(jsonify({"message": "OOPS PLEASE SPECIFY A NAME!"}))
-    #     return render_template('error_new_user.html')
-
-# GET 
-# GET /hello?name=Polly
-# @app.route("/hello")
-# def hello(name=None):
-#     print("VISITING THE HELLO PAGE")
-#     print("REQUEST PARAMS:", dict(request.args))
-
-#     if "name" in request.args:
-#         name = request.args["name"]
-#         message = f"Hello, {name}"
-#     else:
-#         message = "Hello World"
-
-#     # return message
-#     return render_template("hello.html", message=message)
-
+# AAAH I ACTUALLY THINK THE API CLIENT DOESN'T
+# YET MAKE ANY REQUESTS! 
+# SO THAT LAST STEP MIGHT HAVE BEEN UNNECESSARY!
 
 
 # FLASK_APP=app.py flask db init #> generates app/migrations dir
---
+++ web_app/basilica_service.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+import os
+import basilica
+from scipy import spatial
+
+load_dotenv()
+
+# https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey=abc321
+
+BASILICA_API_KEY = os.getenv("BASILICA_API_KEY", default="OOPS")
+
+sentences = [
+    "This is a sentence",
+    "This is a similar sentence",
+    "I don't think this sentence is very similar at all..."
+]
+with basilica.Connection('5b537344-60f8-fc07-1981-05f54ea445b4') as c:
+    embeddings = list(c.embed_sentences((sentences)))
+
+print(list(embeddings))
+
+print(spatial.distance.cosine(embeddings[0], embeddings[1]))
+print(spatial.distance.cosine(embeddings[0], embeddings[2]))
+
+for emb in embeddings:
+    print(type(emb))
+    print(emb)
+    print("---------------")
